afcecc202654d553f7da9ba924fedb3c94716768
KOMAGATA, Wataru <whatarule@gmail.com>
2022-02-19 19:25:37 +0900

    Delete codes for http.

--- src/main.rs
+++ src/main.rs
@@ -17,31 +17,20 @@ type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>
 async fn main() -> Result<()> {
     pretty_env_logger::init();
 
-    // Some simple CLI args requirements...
     let url = match env::args().nth(1) {
         Some(url) => url,
         None => {
             println!("Usage: client <url>");
             return Ok(());
         }
-    };
-
-    // HTTPS requires picking a TLS implementation, so give a better
-    // warning if the user tries to request an 'https' URL.
-    let url = url.parse::<hyper::Uri>().unwrap();
-
-    //if url.scheme_str() != Some("http") {
-    //    println!("This example only works with 'http' URLs.");
-    //    return Ok(());
-    //}
+    }
+    .parse::<hyper::Uri>()
+    .unwrap();
 
     fetch_url(url).await
 }
 
 async fn fetch_url(url: hyper::Uri) -> Result<()> {
-    //let client = Client::new();
-    //let mut res = client.get(url).await?;
-
     let https = HttpsConnector::new();
     let client = Client::builder().build::<_, hyper::Body>(https);
 
@@ -61,8 +50,7 @@ async fn fetch_url(url: hyper::Uri) -> Result<()> {
         .unwrap();
 
     let mut res = client.request(req).await?;
-    //let mut res = client.get(url).await?;
-    //assert_eq!(res.status(), 200);
+    assert_eq!(res.status(), 200);
 
     println!("Response: {}", res.status());
     println!("Headers: {:#?}\n", res.headers());
